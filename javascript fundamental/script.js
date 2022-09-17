b512f451e964fccc81dc06c3b1663ef77fe56293
Shailesh76 <shaileshshrestha76@gmail.com>
2022-09-17 22:54:59 +1000

    my javascript

---
+++ javascript fundamental/script.js
@@ -0,0 +1,194 @@
+console.log("hellow world")
+       // alert("h1");
+
+       /* multi 
+       line 
+       comment
+       8*/
+
+       // declaring variables
+       // var, let, const
+
+    //    var name= "sailesh shrestha";
+    //    let address= "kathmandu nepal";
+    //    const phone= "0425515455";
+
+
+
+    //    console.log(name);
+    //    console.log (address);
+    //    console.log(phone);
+
+       
+       
+
+
+    // ==== operators
+
+    // arithmetic
+     // const total= 5+6;
+    // const total= 6-1;
+    // const total= 5*6;
+    // const total= 5/6;
+    // const total= 5%6;
+// let a=5
+// const b=6
+// const total= a+b;
+//  a+++
+//  console.log(a)
+//  console.log(total);
+
+
+
+// ====== comparison operators
+// ==,===, =>, >=, <, <=
+
+// const v1= 5585544;
+// const v2=555566;
+// // const result= v1==v2;
+// const result= v1<v2
+ 
+
+// data types 
+
+// const val ="string"; // string type
+// let val=565; //string number
+// const val= undefined;  // undefined type
+// const val= null;  //undefined type
+
+// console.log(val);
+// const money= val;
+
+// val=5;
+
+// console.log(val, money);
+
+// console.log(typeof val);
+
+
+
+// ====== object 
+
+// const person={
+//     fname:"sailesh",
+//     phone:0022,
+// };
+// console.log(person);
+
+// const p2=person
+
+// person.address= "djfkjf";
+// console.log(person,p2);
+
+//===== array
+
+// const fruits =[ "apple","banana", 55845];
+
+// ====== function
+// function fun() {}
+
+//==== Math object
+// console.log(Math);
+// const data= Math.PI;
+// const data= Math.round(4,4);
+// const data= Math.pow(5, 3);
+// const data= Math.sqrt(64);
+// const data= Math.ceil(4.7);
+// const data= Math.floor(5.7);
+// const data= Math.min(0, 25, 0, 22);
+// const data= Math.max(225, 255, 555 );
+// const data= Math.random();
+
+
+//===== conditional statement
+
+
+// const age =17;
+// const gender="m"
+// if( age>17) {
+//     console.log("welcome to the oarty");
+// }
+
+// console.log("go home");
+
+
+// settime out         ...................
+// setTimeout(() => {
+    
+// }, timeout);
+
+// setInterval
+
+
+
+//function
+
+
+
+//ES6
+// const add =(a, b) => {
+//     return a + b;
+// };
+// const total = add(14, 5);
+// console.log(total);
+
+
+
+
+// do/while |do{}while{}
+
+
+
+// array
+
+// const fruits= [
+"apple",
+"mango",
+"Orange",
+"tomato"
+// ];
+// console.log(fruits);
+// const lastfruit=fruits.pop();
+// console.log(lastfruit);
+// const subfruit = fruits.slice(1, 3);
+// const subfruit = fruits.splice(2, 4)
+//  fruits.splice(2, 0, "kiwi");
+// fruits.reverse();
+// fruits.sort();
+// console.log(fruits);
+// Loops for Array
+// ->Map, foreach, filter,find,some, every,reduce
+
+//for in, for of
+
+// Map
+
+// const newArg =fruits.map((fruits, index) => {}
+
+// const newArg =fruits.forEach((fruits, index) => {
+
+// const newArg =fruits.filter((fruits, index) => {
+    // console.log(fruits, index);
+    // return !fruits.includes("p");
+// }
+
+
+
+
+
+// Challlenge1:
+//1. Create an array of 50 random numbers range between 1to 100 programmatically.
+//2. Sort the array in descending order
+//3. Create array for odd and even number sets out of the original array.
+//4. get the total value of the array 
+// 5. remove the duplicat value from array.
+
+
+// 1 random number
+Const args=[]
+for(let i =0; i<50; i++) {
+    const num = Math.floor()
+}
+
+
+//challenge 2: create an array of 50 random unique numbers range between 1 to 100programmatically.
